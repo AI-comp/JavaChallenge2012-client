86c252d622a82ac0d86c843d51cd34260cccb4d7
Ryohei Takasawa <henshinfukanou@gmail.com>
2013-03-11 15:56:08 +0900

    add hex and info

--- js/main.js
+++ js/main.js
@@ -47,6 +47,51 @@ var App;
         return Bear;
     })(enchant.Sprite);
     App.Bear = Bear;    
+    var Hex32 = (function (_super) {
+        __extends(Hex32, _super);
+        function Hex32(x, y) {
+                _super.call(this, 32, 32);
+            this.x = x;
+            this.y = y;
+            this.image = Game.game.assets['img/hex32.png'];
+        }
+        return Hex32;
+    })(enchant.Sprite);
+    App.Hex32 = Hex32;    
+    var Hex48 = (function (_super) {
+        __extends(Hex48, _super);
+        function Hex48(x, y) {
+                _super.call(this, 48, 48);
+            this.x = x;
+            this.y = y;
+            this.image = Game.game.assets['img/hex48.png'];
+        }
+        return Hex48;
+    })(enchant.Sprite);
+    App.Hex48 = Hex48;    
+    var Info = (function (_super) {
+        __extends(Info, _super);
+        function Info(x, y, n) {
+            var name = 'img/playerInfo/p' + n + '.png';
+                _super.call(this, 304, 68);
+            this.x = x;
+            this.y = y;
+            this.image = Game.game.assets[name];
+        }
+        return Info;
+    })(enchant.Sprite);
+    App.Info = Info;    
+    var Bank = (function (_super) {
+        __extends(Bank, _super);
+        function Bank(x, y) {
+                _super.call(this, 304, 72);
+            this.x = x;
+            this.y = y;
+            this.image = Game.game.assets['img/bank.png'];
+        }
+        return Bank;
+    })(enchant.Sprite);
+    App.Bank = Bank;    
     var Game = (function (_super) {
         __extends(Game, _super);
         function Game(x, y) {
@@ -56,13 +101,55 @@ var App;
             this.fps = 24;
             this.preload([
                 'images/chara1.png', 
-                'img/map48.png'
+                'img/map48.png', 
+                'img/hex32.png', 
+                'img/hex48.png'
+            ]);
+            this.preload([
+                'img/playerInfo/p0.png', 
+                'img/playerInfo/p1.png', 
+                'img/playerInfo/p2.png', 
+                'img/playerInfo/p3.png', 
+                'img/playerInfo/p4.png', 
+                'img/playerInfo/p5.png'
+            ]);
+            this.preload([
+                'img/bank.png'
             ]);
             this.onload = function () {
                 var field = new Field();
                 var bear = new Bear();
                 _this.rootScene.addChild(bear);
                 _this.rootScene.addChild(field);
+                var i, j;
+                var posx = 128, posy = 12;
+                for(i = 10; i < 20; i++) {
+                    posx = 224 - 24 * (i - 10);
+                    for(j = 0; j < i; j++) {
+                        var hex48 = new Hex48(posx, posy);
+                        _this.rootScene.addChild(hex48);
+                        posx += 48;
+                    }
+                    posy += 36;
+                }
+                for(i = 18; i > 9; i--) {
+                    posx = 224 - 24 * (i - 10);
+                    for(j = 0; j < i; j++) {
+                        var hex48 = new Hex48(posx, posy);
+                        _this.rootScene.addChild(hex48);
+                        posx += 48;
+                    }
+                    posy += 36;
+                }
+                posx = 960;
+                posy = 0;
+                for(i = 0; i < 6; i++) {
+                    var info = new Info(posx, posy, i);
+                    _this.rootScene.addChild(info);
+                    posy += 68;
+                }
+                var bank = new Bank(posx, posy);
+                _this.rootScene.addChild(bank);
             };
         }
         return Game;
--- src/main.ts
+++ src/main.ts
@@ -38,18 +38,93 @@ module App {
     }
   }
 
+  export class Hex32 extends enchant.Sprite {
+    constructor(x: number, y: number) {
+      super(32, 32);
+      this.x = x;
+      this.y = y;
+      this.image = Game.game.assets['img/hex32.png'];
+    }
+  }
+
+  export class Hex48 extends enchant.Sprite {
+    constructor(x: number, y: number) {
+      super(48, 48);
+      this.x = x;
+      this.y = y;
+      this.image = Game.game.assets['img/hex48.png'];
+    }
+  }
+
+  export class Info extends enchant.Sprite {
+    constructor(x: number, y: number, n: number) {
+      var name = 'img/playerInfo/p' + n + '.png';
+
+      super(304, 68);
+      this.x = x;
+      this.y = y;
+      this.image = Game.game.assets[name];
+    }
+  }
+
+  export class Bank extends enchant.Sprite {
+    constructor(x: number, y: number) {
+      super(304, 72);
+      this.x = x;
+      this.y = y;
+      this.image = Game.game.assets['img/bank.png'];
+    }
+  }
+
   export class Game extends enchant.Game {
     public static game: Game;
     constructor(x: number, y: number){
       super(x, y);
       Game.game = this;
       this.fps = 24;
-      this.preload(['images/chara1.png', 'img/map48.png']);
+      this.preload(['images/chara1.png', 'img/map48.png', 'img/hex32.png', 'img/hex48.png']);
+      this.preload(['img/playerInfo/p0.png', 'img/playerInfo/p1.png', 'img/playerInfo/p2.png', 'img/playerInfo/p3.png', 'img/playerInfo/p4.png', 'img/playerInfo/p5.png'])
+      this.preload(['img/bank.png']);
       this.onload = () => {
         var field = new Field();
         var bear = new Bear();
         this.rootScene.addChild(bear);
         this.rootScene.addChild(field);
+
+        var i, j;
+        var posx = 128, posy = 12;
+        for (i = 10; i < 20; i++) {
+          posx = 224 - 24 * (i - 10);
+          for (j = 0; j < i; j++) {
+            var hex48 = new Hex48(posx, posy);
+            this.rootScene.addChild(hex48);
+
+            posx += 48;
+          }
+          posy += 36;
+        }
+          
+        for (i = 18; i > 9; i--) {
+          posx = 224 - 24 * (i - 10);
+          for (j = 0; j < i; j++) {
+            var hex48 = new Hex48(posx, posy);
+            this.rootScene.addChild(hex48);
+            posx += 48;
+          }
+          posy += 36;
+        }
+
+        posx = 960;
+        posy = 0;
+        for (i = 0; i < 6; i++) {
+          var info = new Info(posx, posy, i);
+          this.rootScene.addChild(info);
+
+          posy += 68;
+        }
+
+        var bank = new Bank(posx, posy);
+        this.rootScene.addChild(bank);
       };
     }
   }
