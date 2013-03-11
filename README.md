0c3ca0541876d4612ee74d050d2843aa99aae254
hamadu <dercll1020@gmail.com>
2013-03-11 14:33:13 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 JavaChallenge2012-client
-========================
+=============
 
-JavaChallenge2012 client
+How to compile
+-------------
+tsc --out js/ src/main.ts
+
+
+Testing on browser
+-------------
+open index.html
+
---
+++ src/main.ts
@@ -0,0 +1,61 @@
+///<reference path='types/enchant.d.ts'/>
+declare var enchant : enchant;
+
+// In this context, enchant is module so it is not callable.
+// Use window.enchant instead of 'enchant'.
+interface Window {
+  enchant: Function;
+}
+window.enchant();
+
+module App {
+  export class Field extends enchant.Sprite {
+    constructor() {
+      super(1280, 720);
+      this.x = 0;
+      this.y = 0;
+      this.image = Game.game.assets['img/map48.png'];
+    }
+  }
+
+  export class Bear extends enchant.Sprite {
+    constructor(){
+      super(32, 32);
+      this.x = 32;
+      this.y = 32;
+      this.image = Game.game.assets['images/chara1.png'];
+
+      var self = this;
+      this.on('enterframe', () => self.update());
+    }
+
+    update(): void {
+      var input = Game.game.input
+      if (input.right) this.x += 2;
+      if (input.left)  this.x -= 2;
+      if (input.up)    this.y -= 2;
+      if (input.down)  this.y += 2;
+    }
+  }
+
+  export class Game extends enchant.Game {
+    public static game: Game;
+    constructor(x: number, y: number){
+      super(x, y);
+      Game.game = this;
+      this.fps = 24;
+      this.preload(['images/chara1.png', 'img/map48.png']);
+      this.onload = () => {
+        var field = new Field();
+        var bear = new Bear();
+        this.rootScene.addChild(bear);
+        this.rootScene.addChild(field);
+      };
+    }
+  }
+}
+
+window.onload = () => {
+  var game =  new App.Game(1280, 720);
+  game.start();
+};
